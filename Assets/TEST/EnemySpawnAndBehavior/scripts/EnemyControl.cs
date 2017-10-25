9598440a8acc59b8d63731ec59dd4c669fe9cee7
muffinam007 <aiyetzirah@gmail.com>
2017-10-25 15:41:36 -0700

    enemy movement 90 percent done

--- Assets/TEST/EnemySpawnAndBehavior/scripts/EnemyControl.cs
+++ Assets/TEST/EnemySpawnAndBehavior/scripts/EnemyControl.cs
@@ -12,18 +12,12 @@ public class EnemyControl : MonoBehaviour {
 
 	Rigidbody rb;
 
-	float angleRotation = 10 * Mathf.Deg2Rad;
-	const float MAX_ANGLE_ROTATION = 10 * Mathf.Deg2Rad;
-	float probabilityOfRotation = 0.60f;
-	float nextRotateTimer = 0.0f;
-	float nextRotateTimeLimit;
-	const float LOWER_ROTATE_TIME = 0.2f;
-	const float UPPER_ROTATE_TIME = 0.5f;
+	float angleRotation = 10 * Mathf.Deg2Rad;	
 
 	readonly Vector3 Y_AXIS = new Vector3(0.0f, 1.0f, 0.0f);
 
 	float speed;
-	const float MAX_SPEED = 0.001f;
+	const float MAX_SPEED = 0.01f;
 
 	float probabiliyOfStandingStill = 0.3f;
 
@@ -36,11 +30,7 @@ public class EnemyControl : MonoBehaviour {
 	void Start() {
 		rb = GetComponent<Rigidbody>();
 		moveTimeLimit = Random.Range(LOWER_MOVE_TIME, UPPER_MOVE_TIME);
-		speed = (Random.Range(0.0f, 1.0f) <= probabiliyOfStandingStill ? 0.0f : MAX_SPEED);
-
-		nextRotateTimeLimit = Random.Range(LOWER_ROTATE_TIME, UPPER_ROTATE_TIME);
-		angleRotation = MAX_ANGLE_ROTATION  * (Random.Range(0, 2) == 0 ? 1.0f : -1.0f);
-
+		speed = (Random.Range(0.0f, 1.0f) <= probabiliyOfStandingStill ? 0.0f : MAX_SPEED);		
 	}
 
 	// Update is called once per frame
@@ -61,14 +51,6 @@ public class EnemyControl : MonoBehaviour {
 			moveTimeLimit = Random.Range(LOWER_MOVE_TIME, UPPER_MOVE_TIME);
 		}
 
-		// change in rotation
-		if( (nextRotateTimer += Time.deltaTime) >= nextRotateTimeLimit) {
-			if(Random.Range(0.0f, 1.0f) <= probabilityOfRotation) {
-				transform.Rotate(Y_AXIS, angleRotation);
-			}
-
-			nextRotateTimeLimit = Random.Range(LOWER_ROTATE_TIME, UPPER_ROTATE_TIME);
-			angleRotation = MAX_ANGLE_ROTATION  * (Random.Range(0, 2) == 0 ? 1.0f : -1.0f);
-		}
+		transform.Rotate(Y_AXIS, angleRotation);		
 	}
 }
