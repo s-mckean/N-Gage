fd6860422fe27327415a52e981ae8688f756ffac
hlam1990 <h_lam4@u.pacific.edu>
2017-10-25 19:24:58 -0700

    Player shooting and player prefab. Raycast should be working

---
+++ Assets/Scripts/Playershooting.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Playershooting : MonoBehaviour {
+    public float damage = 10f;
+    public float range = 100f;
+    public Camera playerCam;
+    
+    void Start () {
+		
+	}
+
+    void Update()
+    {
+     
+        if (Input.GetButtonDown("Fire1"))
+        {
+            shotsFired();
+        }
+        
+    }
+
+    void shotsFired()
+    {
+       
+        Debug.Log("shooting");
+        RaycastHit hit;
+        if (Physics.Raycast(playerCam.transform.position, playerCam.transform.forward, out hit, range))
+        {
+            EnemyControl enemy = hit.transform.GetComponent<EnemyControl>();
+            Debug.Log(hit.transform.name);
+            if (enemy != null)
+            {
+                
+            }
+
+        
+        }
+    }
+}
