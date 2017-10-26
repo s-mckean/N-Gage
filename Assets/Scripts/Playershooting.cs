e6098a2df23cc62f0284e37d4a239e261de9e3e4
hlam1990 <h_lam4@u.pacific.edu>
2017-10-25 20:32:59 -0700

    added reloading every 10 shots

--- Assets/Scripts/Playershooting.cs
+++ Assets/Scripts/Playershooting.cs
@@ -4,16 +4,29 @@ using UnityEngine;
 public class Playershooting : MonoBehaviour {
     public float damage = 10f;
     public float range = 100f;
+    public float clipSize = 10;
+    public float reloadTime = 1f;
+
+
     public Camera playerCam;
+
+
+    public GameObject Canvas;
     public GameObject GunShots;
 
+    private float currentClip;
     void Start () {
 		
 	}
 
     void Update()
     {
-     
+
+        if (currentClip <= 0)
+        {
+            StartCoroutine(Reload());
+            return;
+        }
         if (Input.GetButtonDown("Fire1"))
         {
             shotsFired();
@@ -21,9 +34,17 @@ public class Playershooting : MonoBehaviour {
         
     }
 
+    IEnumerator Reload()
+    {
+        Debug.Log("reloading");
+        yield return new WaitForSeconds(reloadTime);
+        currentClip = clipSize;
+    }
+
     void shotsFired()
     {
-       
+
+        currentClip--;
         Debug.Log("shooting");
         RaycastHit hit;
         if (Physics.Raycast(playerCam.transform.position, playerCam.transform.forward, out hit, range))
@@ -37,7 +58,6 @@ public class Playershooting : MonoBehaviour {
 
             GameObject shots = Instantiate(GunShots, hit.point, Quaternion.LookRotation(hit.normal));
             Destroy(shots, 1f);
-
         }
     }
 }
