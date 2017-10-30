d263a67bb37de49daac2cec0067169d5e3b4eecd
Minh Pham <aiyetzirah@gmail.com>
2017-10-30 09:35:14 -0700

    imporve boss behavior. now the dragon chest faced the player when the boss is about to shoot

---
+++ Assets/TEST/EnemySpawnAndBehavior/NormalVectors.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NormalVectors : MonoBehaviour {
+
+	
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log("UP: " + transform.up);
+		Debug.Log("For: " + transform.forward);
+		Debug.Log("Right: " + transform.right);
+	}
+}
