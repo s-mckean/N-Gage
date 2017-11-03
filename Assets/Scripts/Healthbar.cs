32193d3c241173d46ef4b1bfc6cc9a2c324169a4
hlam1990 <h_lam4@u.pacific.edu>
2017-11-02 22:35:33 -0700

    adding hp starting script

---
+++ Assets/Scripts/Healthbar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+ using UnityEngine;
+ using UnityEngine.UI;
+
+public class Healthbar : MonoBehaviour {
+
+    public float hitPoints = 100;
+
+    private float health;
+    // Use this for initialization
+    void Start () {
+        health = hitPoints;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
