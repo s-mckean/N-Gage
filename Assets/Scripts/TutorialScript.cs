cb73b69c4d701358d133e2cef403817fc8bf50d8
Shawn Elpuz <s_elpuz@u.pacific.edu>
2017-11-03 16:49:50 -0700

    Finished tutorial level layout and added Checkpoints and UI

---
+++ Assets/Scripts/TutorialScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialScript : MonoBehaviour {
+
+    public Text TutorialText;
+    public string message;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            TutorialText.text = message;
+        }
+    }
+}
