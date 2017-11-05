745ca92ec466e98a0cda6e618873024b31be518d
Shawn Elpuz <s_elpuz@u.pacific.edu>
2017-11-04 23:47:29 -0700

    Finished tutorial

---
+++ Assets/Scripts/TutorialExit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TutorialExit : MonoBehaviour {
+
+    public string mainMenu;
+
+    private void OnTriggerExit(Collider other)
+    {
+        StartCoroutine(EndTutorial());
+    }
+
+    IEnumerator EndTutorial()
+    {
+        yield return new WaitForSeconds(5);
+        SceneManager.LoadScene(mainMenu);
+    }
+}
---
+++ Assets/Scripts/TutorialOOB.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialOOB : MonoBehaviour {
+
+    public GameObject Checkpoint;
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            other.transform.position = Checkpoint.transform.position;
+            other.transform.rotation = Checkpoint.transform.rotation;
+            other.GetComponent<Rigidbody>().velocity = new Vector3(0,0,0);
+        }
+    }
+}
