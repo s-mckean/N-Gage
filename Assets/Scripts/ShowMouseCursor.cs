e42770b7019974cab7b1bc7b392a8c8d1e758263
muffinam007 <aiyetzirah@gmail.com>
2017-11-05 11:15:41 -0800

    start bg music when game start with the menu scene. add script to make sure mouse cursor is shown

---
+++ Assets/Scripts/ShowMouseCursor.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowMouseCursor : MonoBehaviour {
+
+	private void Awake() {
+		// make sure mouse cursor can be seen
+		 Cursor.lockState = CursorLockMode.None;
+         Cursor.visible = true;
+	}
+}
